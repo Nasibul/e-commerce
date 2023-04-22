91591c3554eeb31e8d8a5504e9a17d26851408f9
Nasibul Hossain <Nasibulh@gmail.com>
2023-04-21 20:20:29 -0400

    3rd LSTM submission

--- Advanced ARIMA/utils.py
+++ Advanced ARIMA/utils.py
@@ -6,6 +6,8 @@ import numpy as np
 from datetime import datetime as dt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_squared_log_error, mean_absolute_percentage_error
+from sklearn.preprocessing import MinMaxScaler
+
 import tensorflow as tf
 from tensorflow import keras
 import keras_tuner
@@ -29,20 +31,26 @@ learning_rates = [0.01, 0.0075, 0.005, 0.0025, 0.001, 0.00075,
                   0.0005, 0.00025, 0.0001, 0.000075, 0.00005, 0.000025, 0.00001]
 
 
-def data_prep(X, Y, forecast=False):
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, Y, test_size=0.10, random_state=5, shuffle=False)
+def data_prep(X, Y, scale=False):
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.10, random_state=5, shuffle=False)
     X_test_copy = X_test
     X_test_index = X_test.index
-    X_train = X_train.values.reshape(X_train.shape[0], X_train.shape[1], 1)
-    X_test = X_test.values.reshape(X_test.shape[0], X_test.shape[1], 1)
-    if forecast == False:
+    if scale == False:
+        X_train = X_train.values.reshape(X_train.shape[0], X_train.shape[1], 1)
+        X_test = X_test.values.reshape(X_test.shape[0], X_test.shape[1], 1)
         y_train = y_train.values.reshape(y_train.shape[0], y_train.shape[1], 1)
         y_test = y_test.values.reshape(y_test.shape[0], y_test.shape[1], 1)
     else:
-        y_train = y_train.values.reshape(
-            y_train.shape[0], y_train.shape[1], 16)
-        y_test = y_test.values.reshape(y_test.shape[0], y_test.shape[1], 16)
+        scaler = MinMaxScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+        y_train = scaler.fit_transform(y_train)
+        y_test = scaler.transform(y_test)
+        X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+        X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+        y_train = y_train.reshape(y_train.shape[0], y_train.shape[1], 1)
+        y_test = y_test.reshape(y_test.shape[0], y_test.shape[1], 1)
+        return X_train, X_test, y_train, y_test, X_test_copy, X_test_index, scaler
     print(X_train.shape)
     print(X_test.shape)
     print(y_train.shape)
@@ -50,18 +58,22 @@ def data_prep(X, Y, forecast=False):
     return X_train, X_test, y_train, y_test, X_test_copy, X_test_index
 
 
-def predict(model, X_train, y_train, X_test, X_test_copy, X_test_index, forecast=False):
-    model.fit(X_train, y_train, epochs=500,
-              verbose=0, callbacks=[es, tensorboard])
-    pred_model = model.predict(X_test)
+def predict(model, X_train, y_train, X_test, X_test_copy, X_test_index, forecast=False, scaler=None):
+    if scaler==None:
+        model.fit(X_train, y_train, epochs=500, verbose=0, callbacks=[es, tensorboard])
+        pred_model = model.predict(X_test)
+    else:
+        es_scale = keras.callbacks.EarlyStopping(monitor='loss', mode='min', patience=10, min_delta=0.005)
+        model.fit(X_train, y_train, epochs=500, verbose=0, callbacks=[es_scale, tensorboard])
+        pred_model = model.predict(X_test)
     if forecast == False:
-        pred = pred_model.reshape(X_test.shape[0], X_test.shape[1])
-        pred = pd.DataFrame(
-            pred, columns=X_test_copy.columns, index=X_test_index)
+        pred = pred_model.reshape(X_test_copy.shape[0], X_test_copy.shape[1])
+        pred = pd.DataFrame(pred, columns=X_test_copy.columns, index=X_test_index)
         pred = pred.iloc[:, -1782:]
     else:
-        pred = pd.DataFrame(
-            pred_model[168].T, columns=train.columns, index=test.index.unique())
+        pred_model = scaler.inverse_transform(pred_model[168].T)
+        pred_model = pred_model.reshape(16, X_test_copy.shape[1])
+        pred = pd.DataFrame(pred_model, columns=train.columns, index=test.index.unique())
         pred = pred.iloc[:, -1782:]
     return pred
 
@@ -90,11 +102,11 @@ def compare(X_test_copy, pred):
         plt.show()
 
 
-def Y_forecast():
+def Y_forecast(X):
     Y = pd.DataFrame()
-    for feature in train.columns:
+    for feature in X.columns:
         for step in range(1, 17):
-            Y[f'{feature}_step_ahead_{step}'] = train[feature].shift(
+            Y[f'{feature}_step_ahead_{step}'] = X[feature].shift(
                 -step, fill_value=0)
     return Y
 
@@ -117,7 +129,7 @@ def submit(pred):
 
 def tuning(model, X_train_mini, y_train_mini, X_val_mini, y_val_mini):
     tuner = keras_tuner.RandomSearch(
-        model, objective='val_loss', max_trials=60)
+        model, objective='val_loss', max_trials=60, overwrite=True, max_consecutive_failed_trials=None)
     tuner.search(X_train_mini, y_train_mini, epochs=10,
                  validation_data=(X_val_mini, y_val_mini))
     best_params = tuner.get_best_hyperparameters()[0]
@@ -146,14 +158,27 @@ def compare_all_series(X_test_copy, pred):
         return results
 
 
-def experiment(X_train, X_test, y_train, y_test, X_test_copy, X_test_index, model, model_number, description):
-    X_train_mini = X_train[:150, :]
-    y_train_mini = y_train[:150, :]
-    X_val_mini = X_train[151:160, :]
-    y_val_mini = y_train[151:160, :]
-    hypertuned_model, best_params = tuning(
-        model, X_train_mini, y_train_mini, X_val_mini, y_val_mini)
-    pred = predict(hypertuned_model, X_train, y_train, X_test, X_test_copy, X_test_index)
+def experiment(X_train, X_test, y_train, y_test, X_test_copy, X_test_index, model, model_number, description, tune=True, forecast=False, scaler=None):
+    if tune==True:
+        y_train_mini = y_train[:150, :]
+        y_val_mini = y_train[151:160, :]
+        if 'output' in model.__code__.co_varnames:
+            X_train_mini = []
+            X_val_mini = []
+            for i in X_train:
+                X_train_mini.append(i[:150, :])
+                X_val_mini.append(i[151:160, :])
+        else:
+            X_train_mini = X_train[:150, :]
+            X_val_mini = X_train[151:160, :]
+        hypertuned_model, best_params = tuning(model, X_train_mini, y_train_mini, X_val_mini, y_val_mini)
+    else:
+        hp = keras_tuner.HyperParameters()
+        hp.Fixed('learning_rate', 0.001)
+        hp.Fixed('optimizer', 'adam')
+        best_params = 'No tuning'
+        hypertuned_model = model(hp)
+    pred = predict(hypertuned_model, X_train, y_train, X_test, X_test_copy, X_test_index, forecast=forecast, scaler=scaler)
     compare(X_test_copy.iloc[:, -1782:], pred)
     results = compare_all_series(X_test_copy.iloc[:, -1782:], pred)
     full_experiment = {'model_number': model_number,
